e47d4c0851b71cab8a93a1b51232b2b83fc8113d
Fake-Keung <wongtsunlok@gmail.com>
2024-08-10 23:25:09 +0800

    updated attribution style

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -7,8 +7,10 @@ export default function Attribution({
   image,
 }: AttributionProps) {
   return (
-    <div className="d-flex align-items-center">
-      <div className="text-tea">{translator.translate(text)}&nbsp;&nbsp;</div>
+    <div className="d-flex align-items-center mx-2">
+      <div className="text-tea mr-1">
+        <small>{translator.translate(text)}</small>
+      </div>
       <div style={{ maxWidth: 30 }}>
         <a href={link}>
           <img className="w-100" src={image} />
